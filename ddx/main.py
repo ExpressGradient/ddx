14b09411a84653b978e789d1cdbf8edb986692b0
ExpressGradient <johndoeknown@protonmail.com>
2024-12-27 21:16:57 +0530

    refines the problem understanding

--- ddx/main.py
+++ ddx/main.py
@@ -4,149 +4,86 @@ from pydantic import BaseModel, Field
 client = OpenAI()
 
 
-class Agent:
-    def __init__(self, id, model):
-        self.id = id
+class DDx:
+    def __init__(self, num_workers=5, model="gpt-4o"):
+        self.n = num_workers
         self.model = model
 
-        self.understanding_consensus = False
-        self.plan = []
-        self.solution = ""
-
-    def _openai_ask(self, query):
-        return (
-            client.chat.completions.create(
-                model=self.model,
-                messages=[
-                    {
-                        "role": "system",
-                        "content": "You are an intelligent agent participating in a group to solve a complex problem",
-                    },
-                    {"role": "user", "content": query},
-                ],
-            )
-            .choices[0]
-            .message.content
-        )
-
-    def _openai_parse(self, query, response_format: BaseModel):
-        return (
-            client.beta.chat.completions.parse(
-                model=self.model,
-                messages=[
-                    {
-                        "role": "system",
-                        "content": "You are an intelligent agent participating in a group to solve a complex problem",
-                    },
-                    {"role": "user", "content": query},
-                ],
-                response_format=response_format,
-            )
-            .choices[0]
-            .message.parsed
-        )
-
-    def _initialize_understanding(self, question):
-        return self._openai_ask(
-            f"""Generate your initial understandings of the following question
-            Question: {question}"""
-        )
-
-    def _analyze_understanding(self, question, understanding):
-        class UnderstandingConsensus(BaseModel):
-            is_satisfied_with_understanding: bool = Field(
-                ...,
-                description="Is satisified with the understanding of the question or not",
-            )
-            feedback: str = Field(
-                ...,
-                description="Feedback of the understanding of the question if not satisfied",
-            )
-
-        understanding_analysis_response = self._openai_parse(
-            f"""Critique the following understanding of the question.
-            It should cover the full picture of the question.
-            It shouldn't miss any important details.
-            Try looking at the question from different angles.
-            If not satisified with it, provide feedback on how it can be improved.
-            Reminder: Goal is to understand the question as best as possible.
-            Question: {question}
-            Understanding: {understanding}""",
-            UnderstandingConsensus,
-        )
-
-        return understanding_analysis_response
-
-    def _update_understanding_with_feedback(self, understanding, feedback):
-        return self._openai_ask(
-            f"""Update the following understanding based on the feedback.
-            Understanding: {understanding}
-            Feedback: {feedback}"""
-        )
-
-
-class DDx:
-    def __init__(self, num_agents=3, model="gpt-4o-mini"):
-        self.agents = [Agent(f"agent-{i}", model) for i in range(num_agents)]
-        
-        self.question = ""
-        self.understanding_artifact = ""
-        
-        self.max_understanding_iterations = 5
+        self.problem = ""
+        self.__understanding = ""
 
         self.verbose = False
 
-    def _vprint(self, message):
-        if self.verbose:
-            print(message, "\n")
+    def __chat(self, input_message: str, n=None) -> list[str]:
+        if n is None:
+            n = self.n
 
-    def _create_understanding(self):
-        self._vprint("Understanding the question...")
-        self.understanding_artifact = self.agents[0]._initialize_understanding(
-            self.question
+        response = client.chat.completions.create(
+            model=self.model,
+            messages=[
+                {
+                    "role": "developer",
+                    "content": "You are a highly intelligent assistant that can help in solving a complex problem in multiple phases",
+                },
+                {
+                    "role": "user",
+                    "content": input_message,
+                },
+            ],
+            n=n,
         )
-        self._vprint(f"Initial understanding: {self.understanding_artifact}")
 
-    def _critique_understanding(self):
-        self._vprint("Critiquing the understanding...")
+        return [choice.message.content for choice in response.choices]
 
-        iteration = 0
+    def __vprint(self, content: str):
+        if self.verbose:
+            print(content, "\n")
 
-        while not all([agent.understanding_consensus for agent in self.agents]):
-            for agent in self.agents:
-                if not agent.understanding_consensus:
-                    critique = agent._analyze_understanding(
-                        self.question, self.understanding_artifact
-                    )
+    def __understand_problem(self):
+        self.__vprint("Generating problem understanding candidates...")
 
-                    if critique.is_satisfied_with_understanding:
-                        agent.understanding_consensus = True
-                        self._vprint(f"{agent.id} is satisfied with the understanding.")
-                    else:
-                        self.understanding_artifact = (
-                            agent._update_understanding_with_feedback(
-                                self.understanding_artifact, critique.feedback
-                            )
-                        )
-                        self._vprint(
-                            f"{agent.id} is not satisfied with the understanding."
-                        )
-                        self._vprint(f"Feedback: {critique.feedback}")
+        candidates = self.__chat(f"""Goal: Grasp the following problem fully to set the foundation for a solution.
 
-            iteration += 1
+Sub-steps: 
+Comprehend the Problem Statement
+    - Break it down into smaller components
+    - What is being asked? What does "solved" look like?
+Identify Key Details
+    - What information is provided?
+    - What are the important variables, relationships or elements?
+Clarify Ambiguities
+    - Identify areas of vagueness or missing information
+    - List down assumptions
+Define Sucess Criteria:
+    - Specify what constitutes a successful solution
 
-            if iteration >= self.max_understanding_iterations:
-                self._vprint("Max understanding iterations reached.")
-                break
+Problem: {self.problem}""")
 
-        self._vprint("Understanding consensus reached.")
-        self._vprint(f"Final understanding: {self.understanding_artifact}")
+        self.__vprint("Merging the problem understanding candidates..")
 
-    def run(self, question, max_understanding_iterations=5, verbose=False):
-        self.question = question
+        self.__understanding = self.__chat(
+            f"""Goal: Merge the following problem understanding candidates into a single, comprehensive understanding.
+The final understanding should be:
+1. Complete: covers all important details of the problem
+2. Clear: Easily to understand and logically structured.
+3. Consistent: Resolves any conflicts or contradictions between the candidates.
+4. Accurate: Aligns with the original problem statement.
 
-        self.max_understanding_iterations = max_understanding_iterations
+Steps to follow:
+1. Identify common elements across the candidates and include them in the final understanding.
+2. Incorporate unique or complementary details from each candidate to enrich the final understanding.
+3. Resolve any conflicting or ambiguous points by providing clear explanations or assumptions.
+4. Present the final understanding in a structured and concise format.
+
+Here are the problem understanding candidates: {"\n-----\n".join(candidates)}""",
+            n=1,
+        )[0]
+
+        self.__vprint(f"Problem understanding: {self.__understanding}")
+
+    def run(self, problem: str, verbose=False):
+        self.problem = problem
         self.verbose = verbose
 
-        self._create_understanding()
-        self._critique_understanding()
+        self.__understand_problem()
+        return ""
--- eval/basic.py
+++ eval/basic.py
@@ -1,12 +1,14 @@
 import sys
 import os
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from ddx import DDx
 
 
+ddx = DDx(model="gpt-4o")
 
-ddx = DDx(model='gpt-4o')
-
-ddx.run("Nondimensionalize the polynomial\\[a_{1} x^{12} + a_{2} x^{8} + a_{3}\\]into one of the form $\\epsilon y^{12} + y^{8} + 1. $Express $\\epsilon$ as a function of $a_1$, $a_2$, and $a_3.$", verbose=True)
+ddx.run(
+    "Consider the polynomial\\[P(x) =\\epsilon x^{8} + x^{2} - 1.\\] \nFind first order approximations for all roots of the polynomial in the limit of small positive $\\epsilon$ and large positive $\\epsilon$.",
+    verbose=True,
+)
